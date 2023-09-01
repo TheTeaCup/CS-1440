3c6e713ddc82c57295ae41081235a78d2a812dbe
Tea Cup <lw0482352@gmail.com>
2023-09-01 16:15:22 +0000

    Strings and Comments

---
+++ week_one/StringsAndComments.java
@@ -0,0 +1,11 @@
+public class StringAndComments
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello!"); // String literal
+        String name = "Hunter";
+        String greeting = "Hello, ";
+        String message = greeting + name;
+        System.out.println(message);
+    }
+}
