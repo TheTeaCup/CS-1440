2e92aadbd48f8fb172ae46408c69f3ad105d4ca4
Tea Cup <lw0482352@gmail.com>
2023-09-06 16:10:26 +0000

    KeyBoard.java

---
+++ week_two/KeyBoard.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Enter a character: ");
+        int c = System.in.read();
+        System.out.println((char)c);
+        
+        // scanner
+        Scanner kb = new Scanner(System.in);
+        
+        
+    }
+}
