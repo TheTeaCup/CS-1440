c8329b5d252de52f9039c8bbd5b3e8cc33675ed2
Tea Cup <lw0482352@gmail.com>
2023-09-15 16:33:34 +0000

    packages

---
+++ week_three/Circle2.java
@@ -0,0 +1,20 @@
+public class Circle2
+{
+    private double radius;  // field
+                            // accessible in the class
+    
+    public double getRadius()
+    {
+        return radius;
+    }
+    
+    public void setRadius(double r)
+    {
+        int i = 10;
+        
+        double radius;
+        System.out.println("param r: " + r);
+        radius = r;
+        
+    }
+}
---
+++ week_three/Packages.md
@@ -0,0 +1,12 @@
+# Packages
+
+Uses Java API
+
+package /
+    - java.util (need to import)
+        - Scanner
+        - ArrayList
+        - ...
+    - java.lang (don't have to import, automatically done)
+        - String 
+        - System
