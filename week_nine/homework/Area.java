bf5b34cb6001394cffb0ececf6b47e8356f749b0
TheTeaCup <lw0482352@gmail.com>
2023-10-23 19:42:44 -0400

    Week Nine Homework Area done

--- week_nine/homework/Area.java
+++ week_nine/homework/Area.java
@@ -1,14 +1,27 @@
 /**
  * The Area class.
- * 
+ *
  * This program has three overloaded static methods 
  * for calculating the areas of the circles, 
  * rectangles, and cylinders.
  *
- * @author (your name) 
- * @version (a version number or a date)
+ * @author hunter.wilson
+ * @version 10.23.2023
  */
 public class Area
 {
-    
+    // Circle
+    public static double getArea(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    // Rectangle
+    public static double getArea(int width, int length) {
+        return width * length;
+    }
+
+    // Cylinder
+    public static double getArea(double radius, double height) {
+        return Math.PI * Math.pow(radius, 2) * height;
+    }
 }
