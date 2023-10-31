cfc31a8e5838e22237a23d8b34c15f22338fc05a
TheTeaCup <lw0482352@gmail.com>
2023-10-30 21:24:04 -0400

    Week 10

---
+++ week_ten/Demo.java
@@ -0,0 +1,53 @@
+
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        Student tim = new Student("900", "Tim");
+        System.out.println(tim);
+        System.out.println("Ordinal for Tim: " + tim.getYear().ordinal());
+
+        Student taylor = new Student("901", "Taylor", StudentYear.JUNIOR);
+        System.out.println(taylor);
+        System.out.println("Ordinal for Taylor: " + taylor.getYear().ordinal());
+
+        StudentYear timYear = tim.getYear();
+        StudentYear taylorYear = taylor.getYear();
+        if (timYear.equals(taylorYear))
+        {
+            System.out.println("They are in the same year!");
+        }
+
+        else if (timYear.compareTo(taylorYear) > 0)
+        {
+            System.out.println("Tim is closer to graduating!");
+        }
+        else
+        {
+            System.out.println("Taylor is closer to graduating!");
+        }
+
+        switch (taylorYear)
+        {
+            case FRESHMAN:
+                System.out.println("Taylor is a first-year student");
+                break;
+
+            case SOPHOMORE:
+                System.out.println("Taylor is a second-year student");
+                break;
+
+            case JUNIOR:
+                System.out.println("Taylor is a third-year student");
+                break;
+
+            case SENIOR:
+                System.out.println("Taylor is a fourth-year student");
+                break;
+
+            case GRADUATE:
+                System.out.println("Taylor is a graduate student");
+                break;
+        }
+    }
+}
---
+++ week_ten/Student.java
@@ -0,0 +1,37 @@
+
+public class Student
+{
+    private String id;
+    private String name;
+    private StudentYear year;
+
+    public Student(String id, String name, StudentYear year)
+    {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+    }
+
+    public Student(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+        year = StudentYear.FRESHMAN;
+    }
+
+    public StudentYear getYear()
+    {
+        return year;
+    }
+
+    public String toString()
+    {
+        return String.format(
+                "%s, %s, %s",
+                id, name, year
+        );
+    }
+
+
+
+}
